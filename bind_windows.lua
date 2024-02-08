1b989dccf52f6d20680a7f91dfcc0b91ff1685cb
Adrian Brady <ajbrady999@protonmail.com>
2024-02-08 14:29:26 -0600

    initial commit

---
+++ bind_windows.lua
@@ -0,0 +1,64 @@
+-- osascript -e 'id of app "App Name"'
+local windows = {
+    kitty = {
+        modifier = 'option',
+        key = 'return',
+        bundle_ID = '',
+        title = 'kitty',
+        manage_size = false,
+        frame_properties = {}
+    },
+
+    messages = {
+        modifier = 'option',
+        key = 'm',
+        bundle_ID = '',
+        title = 'messages',
+        manage_size = false,
+        frame_properties = {}
+    },
+
+    obsidian = {
+        modifier = 'option',
+        key = 'o',
+        bundle_ID = '',
+        title = 'obsidian',
+        manage_size = false,
+        frame_properties = {}
+    },
+
+    ulysses = {
+        modifier = 'option',
+        key = 'u',
+        bundle_ID = '',
+        title = 'UlyssesMac',
+        manage_size = false,
+        frame_properties = {},
+    },
+
+    things3 = {
+        modifier = 'option',
+        key = 't',
+        bundle_ID = '',
+        title = 'Things3',
+        manage_size = false,
+        frame_properties = {},
+    }
+
+}
+
+for k, v in pairs(windows) do
+    local command = "osascript -e 'id of app \"".. v["title"] .. "\"\'"
+    local file = io.popen(command, "r")
+    local output = file:read("*a"):gsub("\n", "")
+    file:close()
+    v["bundle_ID"] = output
+    print("Registered app: " .. v["title"])
+    print("  === On keys: " .. v["modifier"] .. " + " .. v["key"])
+    print("  === On bundleID: " .. v["bundle_ID"])
+    print("  === Managed Size: " .. tostring(v["manage_size"]))
+    print()
+
+end
+
+return windows
